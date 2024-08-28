a8ef67ec10da730b52b8215714361c481ea2535d
mynor hidalgo <stmhida6@upana.edu.gt>
2024-08-28 11:11:00 -0600

    creando productor

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Cola cola = new Cola(10); // tamaño de la cola
 
-        cola.meter(1);
+       /* cola.meter(1);
         cola.meter(2);
         cola.meter(3);
 
@@ -15,7 +15,20 @@ public class Main {
         }
         for(int i = 0; i < 100; i++) {
             System.out.println(cola.sacar());
+        }*/
+
+        Productor productor = new Productor(cola, 10);
+        try {
+            Thread hiloProductor = new Thread(productor);
+            hiloProductor.start();
+            hiloProductor.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
+
+
+
 
 
 
--- src/Productor.java
+++ src/Productor.java
@@ -1,5 +1,24 @@
 
+import java.util.Random;
+public class Productor  implements Runnable {
+    private final Cola cola;
+    private final int cantidad;
 
-public class Productor  {
+    public Productor(Cola cola, int cantidad) {
+        this.cola = cola;
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public void run() {
+        Random random = new Random();
+        //generar valores aleatorios
+        System.out.println("Productor iniciado");
+        for (int i = 0; i < cantidad; i++) {
+            int numero = random.nextInt(100);
+            cola.meter(numero);
+            System.out.println("metio: " + numero);
+        }
+    }
 
 }
